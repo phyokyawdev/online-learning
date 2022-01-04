64f812b91c364d63e51789357680d999a09c9d93
Phyo Kyaw <phyokyawdev@gmail.com>
2022-01-04 16:59:02 +0630

    update auth routes

--- src/routes/v1/auth/login.js
+++ src/routes/v1/auth/login.js
@@ -1,39 +1,29 @@
 const express = require("express");
 const passport = require("passport");
-const { oneOf, body } = require("express-validator");
 const router = express.Router();
 
 const { NotAuthorizedError } = require("@shared/errors");
 const { validateRequest } = require("@shared/middlewares");
 const { setAuthTokenToRequest } = require("@shared/services/auth-token");
+const { loginRules } = require("@models/user");
 
-router.post(
-  "/login",
-  validateRequest([
-    oneOf([
-      body("username").isLength({ min: 5, max: 30 }),
-      body("email").isEmail(),
-    ]),
-    body("password").isLength({ min: 8, max: 1024 }),
-  ]),
-  (req, res, next) => {
-    passport.authenticate("login", async (err, user, info) => {
-      if (err) return next(err);
+router.post("/login", validateRequest(loginRules), (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    if (err) return next(err);
 
-      if (!user) return next(new NotAuthorizedError(info.message));
+    if (!user) return next(new NotAuthorizedError(info.message));
 
-      let token;
-      try {
-        token = await user.generateAuthToken();
-      } catch (err) {
-        return next(err);
-      }
+    let token;
+    try {
+      token = await user.generateAuthToken();
+    } catch (err) {
+      return next(err);
+    }
 
-      setAuthTokenToRequest(req, token);
+    setAuthTokenToRequest(req, token);
 
-      res.send(user);
-    })(req, res, next);
-  }
-);
+    res.send(user);
+  })(req, res, next);
+});
 
 module.exports = router;
--- src/routes/v1/auth/signup.js
+++ src/routes/v1/auth/signup.js
@@ -1,37 +1,29 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const { body } = require("express-validator");
 
 const { ConflictError } = require("@shared/errors");
 const { validateRequest } = require("@shared/middlewares");
 const { setAuthTokenToRequest } = require("@shared/services/auth-token");
+const { signupRules } = require("@models/user");
 
-router.post(
-  "/signup",
-  validateRequest([
-    body("username").isLength({ min: 5, max: 30 }),
-    body("email").isEmail(),
-    body("password").isLength({ min: 8, max: 1024 }),
-  ]),
-  (req, res, next) => {
-    passport.authenticate("signup", async (err, user, info) => {
-      if (err) return next(err);
+router.post("/signup", validateRequest(signupRules), (req, res, next) => {
+  passport.authenticate("signup", async (err, user, info) => {
+    if (err) return next(err);
 
-      if (!user) return next(new ConflictError(info.message));
+    if (!user) return next(new ConflictError(info.message));
 
-      let token;
-      try {
-        token = await user.generateAuthToken();
-      } catch (err) {
-        return next(err);
-      }
+    let token;
+    try {
+      token = await user.generateAuthToken();
+    } catch (err) {
+      return next(err);
+    }
 
-      setAuthTokenToRequest(req, token);
+    setAuthTokenToRequest(req, token);
 
-      res.status(201).send(user);
-    })(req, res, next);
-  }
-);
+    res.status(201).send(user);
+  })(req, res, next);
+});
 
 module.exports = router;
