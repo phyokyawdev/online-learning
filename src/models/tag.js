05ba7a924231167048c27a5613054ae3bdee7ff6
Phyo Kyaw <phyokyawdev@gmail.com>
2022-01-05 18:39:16 +0630

    add tag model

---
+++ src/models/tag.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const { body, query } = require("express-validator");
+
+/**
+ * Tag
+ * ===
+ * name
+ */
+const tagSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 35,
+      unique: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
+
+tagSchema.statics.isExistingTagName = async function (name) {
+  const tag = await this.findOne({ name });
+  if (!tag) return false;
+  return true;
+};
+
+const Tag = mongoose.model("Tag", tagSchema);
+
+/** Validation rules */
+const createRules = [
+  body("name")
+    .trim()
+    .not()
+    .isEmpty()
+    .bail()
+    .isAlpha()
+    .toLowerCase()
+    .isLength({ max: 35 }),
+];
+
+const readRules = [
+  query("offset").customSanitizer(parseInt).default(0),
+  query("limit").customSanitizer(parseInt).default(20),
+];
+
+module.exports = { Tag, createRules, readRules };
