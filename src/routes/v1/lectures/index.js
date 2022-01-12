b4fc0634d3f5313de67edfda946eab485e8161bc
Phyo Kyaw <phyokyawdev@gmail.com>
2022-01-12 22:20:47 +0630

    refactor lectures routes

--- src/routes/v1/lectures/index.js
+++ src/routes/v1/lectures/index.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
-const createRouter = require("./create");
-const updateRouter = require("./update");
-const deleteRouter = require("./delete");
+const { allowCourseOwner, validateRequest } = require("@shared/middlewares");
+const { createRules, Lecture } = require("@models/lecture");
+const { NotFoundError } = require("@shared/errors");
 
-router.use(createRouter);
-router.use(updateRouter);
-router.use(deleteRouter);
+/**
+ * req.lecture will be available in
+ * route handlers with id param.
+ */
+router.param("id", async (req, _res, next, id) => {
+  const lecture = await Lecture.findByIdString(id);
+  if (!lecture) throw new NotFoundError("Lecture not exist.");
+  req.lecture = lecture;
+  next();
+});
+
+router.post(
+  "/",
+  allowCourseOwner,
+  validateRequest(createRules),
+  async (req, res) => {
+    const { body, currentCourse, user } = req;
+    const lecture = await Lecture.create(body, currentCourse.id, user.id);
+    res.status(201).send(lecture);
+  }
+);
+
+router.put(
+  "/:id",
+  allowCourseOwner,
+  validateRequest(createRules),
+  async (req, res) => {
+    const { body, lecture } = req;
+    await lecture.updateBody(body);
+    res.send(lecture);
+  }
+);
+
+router.delete("/:id", allowCourseOwner, async (req, res) => {
+  const { lecture } = req;
+  await lecture.remove();
+  res.status(204).send();
+});
 
 module.exports = router;
