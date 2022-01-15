b1adc7d570c9f962cc2ae3e17751abf85ea2401a
Phyo Kyaw <phyokyawdev@gmail.com>
2022-01-15 16:57:59 +0630

    add assignment question model

--- src/models/assignment_question.js
+++ src/models/assignment_question.js
@@ -1,33 +1,83 @@
+const { body } = require("express-validator");
 const mongoose = require("mongoose");
-const { Course } = require("@models/course");
 
 /**
  * AssignmentQuestion
  * ==========
- * question
- * deadline
+ * index
+ * title
+ * content
  *
  * course
  */
-const assignmentQuestionSchema = new mongoose.Schema({
-  question: {
-    type: String,
-    required: true,
+const questionSchema = new mongoose.Schema(
+  {
+    index: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
   },
-  deadline: {
-    type: Date,
-    required: true,
-  },
-  course: {
-    type: mongoose.Types.ObjectId,
-    ref: Course,
-    required: true,
-  },
-});
-
-const AssignmentQuestion = mongoose.model(
-  "AssignmentQuestion",
-  assignmentQuestionSchema
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
 );
 
-module.exports = { AssignmentQuestion };
+/** Static methods */
+questionSchema.statics.findByIdString = async function (id) {
+  if (!mongoose.isValidObjectId(id)) return false;
+  const question = await this.findById(id);
+  return question;
+};
+
+questionSchema.statics.create = async function (body, course) {
+  const { index, title, content } = body;
+  const question = new this({ index, title, content, course });
+  await question.save();
+  return question;
+};
+
+questionSchema.statics.findByQuery = async function (query, course) {
+  const questions = await this.find({ course });
+  return questions;
+};
+
+/** Instance methods */
+questionSchema.methods.updateBody = async function (body) {
+  const { index, title, content } = body;
+  this.index = index;
+  this.title = title;
+  this.content = content;
+  await this.save();
+  return this;
+};
+
+const AssignmentQuestion = mongoose.model("AssignmentQuestion", questionSchema);
+
+/** Validation rules */
+const createRules = [
+  body("index").isNumeric(),
+  body("title").isString().trim().notEmpty(),
+  body("content").isString().trim().notEmpty(),
+];
+
+module.exports = { AssignmentQuestion, createRules };
