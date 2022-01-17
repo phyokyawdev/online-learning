5edd4975eadd12c83c5607aff3d0befe759cb314
Phyo Kyaw <phyokyawdev@gmail.com>
2022-01-17 18:06:09 +0630

    add assignment_answer model

--- src/models/assignment_answer.js
+++ src/models/assignment_answer.js
@@ -1,45 +1,108 @@
+const { body, query } = require("express-validator");
 const mongoose = require("mongoose");
-const { AssignmentQuestion } = require("@models/assignment_question");
-const { UserCourse } = require("@models/user_course");
 
 /**
  * AssignmentAnswer
- * =======================
+ * ================
  * answer
  * date
  * score
  *
- * user_course
+ * student
  * assignment_question
  */
-const assignmentAnswerSchema = new mongoose.Schema({
-  answer: {
-    type: String,
-    required: true,
+const answerSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: new Date(),
+      required: true,
+    },
+    score: {
+      type: Number,
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseStudent",
+      required: true,
+    },
+    assignment_question: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AssignmentQuestion",
+      required: true,
+    },
   },
-  date: {
-    type: Date,
-    default: new Date(),
-    required: true,
-  },
-  score: {
-    type: Number,
-  },
-  user_course: {
-    type: mongoose.Types.ObjectId,
-    ref: UserCourse,
-    required: true,
-  },
-  assignment_question: {
-    type: mongoose.Types.ObjectId,
-    ref: AssignmentQuestion,
-    required: true,
-  },
-});
-
-const AssignmentAnswer = mongoose.model(
-  "AssignmentAnswer",
-  assignmentAnswerSchema
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
 );
 
-module.exports = { AssignmentAnswer };
+answerSchema.index({ student: 1, assignment_question: 1 }, { unique: true });
+
+/** Static methods */
+answerSchema.statics.findByIdString = async function (id) {
+  if (!mongoose.isValidObjectId(id)) return false;
+  const answer = await this.findById(id);
+  return answer;
+};
+
+answerSchema.statics.isExisting = async function (
+  student,
+  assignment_question
+) {
+  const answer = await this.findOne({ student, assignment_question });
+  if (!answer) return false;
+  return answer;
+};
+
+answerSchema.statics.create = async function (
+  body,
+  student,
+  assignment_question
+) {
+  const { content } = body;
+  const answer = new this({ content, student, assignment_question });
+  await answer.save();
+  return answer;
+};
+
+answerSchema.statics.findByQuery = async function (query, assignment_question) {
+  const { offset, limit } = query;
+  const answers = await this.find({ assignment_question })
+    .skip(offset)
+    .limit(limit);
+  return answers;
+};
+
+/** Instance methods */
+answerSchema.methods.updateBody = async function (body) {
+  const { score } = body;
+  this.score = score;
+  await this.save();
+  return this;
+};
+
+const AssignmentAnswer = mongoose.model("AssignmentAnswer", answerSchema);
+
+/** Validation rules */
+const createRules = [body("content").isString().trim().notEmpty()];
+
+const readRules = [
+  query("offset").customSanitizer(parseInt).default(0),
+  query("limit").customSanitizer(parseInt).default(10),
+];
+
+const updateRules = [body("score").isNumeric()];
+
+module.exports = { AssignmentAnswer, createRules, readRules, updateRules };
